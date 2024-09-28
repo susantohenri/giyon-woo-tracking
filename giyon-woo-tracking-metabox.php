2d1b35df2d9757970fa7e888f2ef1b2940b53e7c
Henri Susanto <henry.dinus@gmail.com>
2024-09-29 06:30:55 +0700

    meta box

---
+++ giyon-woo-tracking-metabox.php
@@ -0,0 +1,19 @@
+<ul class="order_actions submitbox">
+
+    <li class="wide">
+        Add, edit, or remove the tracking info. Once done, click on the "save order" button to update order tracking info.
+    </li>
+
+    <li class="wide">
+        <b>Shipping Company</b>
+        <br>
+        <select style="width: 100%;">
+            <option>Japan Post</option>
+        </select>
+    </li>
+
+    <li class="wide">
+        <button type="submit" class="button button-primary" name="save" value="Update">Update</button>
+    </li>
+
+</ul>
--- giyon-woo-tracking.php
+++ giyon-woo-tracking.php
@@ -12,3 +12,21 @@
  * Requires at least: 6.5
  * Requires PHP: 8.1.29
  */
+
+add_action('add_meta_boxes', 'giyon_tracking_meta_box');
+function giyon_tracking_meta_box()
+{
+    add_meta_box(
+        'giyon-woo-tracking',
+        __('Tracking Code'),
+        'giyon_tracking_meta_box_content',
+        'woocommerce_page_wc-orders',
+        'side',
+        'default'
+    );
+}
+
+function giyon_tracking_meta_box_content()
+{
+    include_once(plugin_dir_path(__FILE__) . 'giyon-woo-tracking-metabox.php');
+}
