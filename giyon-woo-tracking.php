745eceabcaeea69152b53d9ac3c01ae7be776d7d
Henri Susanto <henry.dinus@gmail.com>
2024-09-29 05:49:37 +0700

    init

---
+++ giyon-woo-tracking.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: Giyon Woo Tracking
+ * Plugin URI: https://github.com/susantohenri/giyon-woo-tracking
+ * Description: Custom WooCommerce Tracking Plugin for Giyon.
+ * Version: 1.0.0
+ * Author: Henrisusanto
+ * Author URI: https://github.com/susantohenri/
+ * Text Domain: giyon-woo-tracking
+ * Domain Path: /i18n/languages/
+ * Requires at least: 6.5
+ * Requires PHP: 8.1.29
+ */
