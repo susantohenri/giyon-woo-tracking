6613502430071bbc09754b2cce06359d5b31c802
Henri Susanto <henry.dinus@gmail.com>
2024-09-29 10:07:27 +0700

    ensure: done

--- giyon-woo-tracking.php
+++ giyon-woo-tracking.php
@@ -10,10 +10,14 @@
  * Text Domain: giyon-woo-tracking
  * Domain Path: /i18n/languages/
  * Requires at least: 6.5
- * Requires PHP: 8.1.29
+ * Requires PHP: 8.0.30
  */
 
 add_action('add_meta_boxes', 'giyon_tracking_meta_box');
+add_filter('manage_woocommerce_page_wc-orders_columns', 'giyon_tracking_column_header');
+add_action('manage_woocommerce_page_wc-orders_custom_column', 'giyon_tracking_column_content', 10, 2);
+add_action('woocommerce_email_before_order_table', 'giyon_tracking_email_complete_order', 20, 4);
+
 function giyon_tracking_meta_box()
 {
     add_meta_box(
@@ -29,4 +33,19 @@ function giyon_tracking_meta_box()
 function giyon_tracking_meta_box_content()
 {
     include_once(plugin_dir_path(__FILE__) . 'giyon-woo-tracking-metabox.php');
+}
+
+function giyon_tracking_column_header($columns)
+{
+    return array_merge($columns, ['verified' => __('Tracking', 'textdomain')]);
+}
+
+function giyon_tracking_column_content($column_key, $order)
+{
+    if ($column_key == 'verified') echo $order->id;
+}
+
+function giyon_tracking_email_complete_order($order, $sent_to_admin, $plain_text, $email)
+{
+    echo '<h2 class="email-upsell-title">Lorem Ipsum sit Amet</p>';
 }
